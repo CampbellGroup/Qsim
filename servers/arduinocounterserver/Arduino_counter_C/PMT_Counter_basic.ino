3790910e40c3ff9383692e085187fb53ef718071
tony <ransfordanthony@gmail.com>
2014-05-18 08:03:10 -0700

    added arduino c code

---
+++ servers/arduinocounterserver/Arduino_counter_C/PMT_Counter_basic.ino
@@ -0,0 +1,16 @@
+/* PMT counter basic v 1.0
+ */
+#include <FreqCount.h>
+
+void setup() {
+  Serial.begin(57600);
+  FreqCount.begin(100);
+}
+
+void loop() {
+  if (FreqCount.available()) {
+    unsigned long count = FreqCount.read();
+    Serial.write(count);
+  }
+}
+
