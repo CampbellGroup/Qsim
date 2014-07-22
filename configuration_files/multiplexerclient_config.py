d884fff41f8879eccb4b3a43d3cff940a8eafdd9
aransfor <ransfordanthony@gmail.com>
2014-07-22 16:16:00 -0700

    adjusted config

--- configuration_files/multiplexerclient_config.py
+++ configuration_files/multiplexerclient_config.py
@@ -4,7 +4,7 @@ class multiplexer_config(object):
     info is the configuration dictionary in the form
     {channel_name: (port, hint, display_location, stretched))} Note: (0,0) location reserved for lock switch
     '''
-    info = {'Channel 5': (5, '405.645680', (1,1), False),
-            'Channel 6': (6, '320.571975', (1,2), False),
-            'Channel 7': (7, '751.526150', (1,3), False)
+    info = {'369': (5, '405.645680', (1,1), False),
+            '935': (6, '320.571975', (1,2), False),
+            '399': (7, '751.526150', (1,3), False)
             }
