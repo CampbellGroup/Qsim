17aa003df833706e5e8e773a1b8f73f3ad678349
aransfor <ransfordanthony@gmail.com>
2015-12-21 13:10:52 -0800

    removed unused import and added pep8 stuff

--- clients/laserquantumpump/pumpclient.py
+++ clients/laserquantumpump/pumpclient.py
@@ -10,7 +10,7 @@ class PumpClient(QtGui.QWidget):
     def __init__(self, reactor, cxn = None):
         super(PumpClient, self).__init__()
         self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
-	self.cxn = cxn
+        self.cxn = cxn
         self.reactor = reactor         
         self.connect()
         
@@ -20,11 +20,10 @@ class PumpClient(QtGui.QWidget):
         connects incoming signals to relavent functions
         
         """
-        from labrad.wrappers import connectAsync
-	if self.cxn is None:
+        if self.cxn is None:
             self.cxn = connection(name='Pump Client')
             yield self.cxn.connect()
-	self.server = yield self.cxn.get_server('laserquantumpumpserver')
+        self.server = yield self.cxn.get_server('laserquantumpumpserver')
         #self.server = yield self.cxn.laserquantumpumpserver 
 
         yield self.server.signal__current_changed(SIGNALID1)
@@ -37,41 +36,39 @@ class PumpClient(QtGui.QWidget):
        
     def initializeGUI(self):  
         layout = QtGui.QGridLayout() 
-	font = QtGui.QFont()
-	font.setBold(True)
-	font.setPointSize(30)
-	self.title = QtGui.QLabel('Laser Quantum Pump Laser')
-	self.title.setFont(font)
-	self.title.setAlignment(QtCore.Qt.AlignCenter)
-	self.currentlabel = QtGui.QLabel('Current')
-	self.powerlabel = QtGui.QLabel('Power')
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setPointSize(30)
+        self.title = QtGui.QLabel('Laser Quantum Pump Laser')
+        self.title.setFont(font)
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.currentlabel = QtGui.QLabel('Current')
+        self.powerlabel = QtGui.QLabel('Power')
 
-	self.currentprogbar = QtGui.QProgressBar()
-	self.currentprogbar.setGeometry(30, 40, 200, 25)
+        self.currentprogbar = QtGui.QProgressBar()
+        self.currentprogbar.setGeometry(30, 40, 200, 25)
 
-	self.powerprogbar = QtGui.QProgressBar()
-	self.powerprogbar.setGeometry(30, 40, 200, 25)
-	self.powerprogbar.setMaximum(100)
-	self.powerprogbar.setMinimum(0)
+        self.powerprogbar = QtGui.QProgressBar()
+        self.powerprogbar.setGeometry(30, 40, 200, 25)
+        self.powerprogbar.setMaximum(100)
+        self.powerprogbar.setMinimum(0)
 
-	layout.addWidget(self.title,0,0)
+        layout.addWidget(self.title,0,0)
         layout.addWidget(self.currentprogbar, 2,0)
         layout.addWidget(self.powerprogbar, 4,0)
         layout.addWidget(self.currentlabel, 1,0)
         layout.addWidget(self.powerlabel, 3,0)
 
-
         self.setLayout(layout)
-        from twisted.internet.reactor import callLater
 
     def updateCurrent(self,c,  current):
-	self.currentprogbar.setValue(current)
+        self.currentprogbar.setValue(current)
 
     def updatePower(self, c, power):
-	powerperc = power['W']*100/8.0
-	self.powerprogbar.setValue(powerperc)
-	self.powerprogbar.setFormat(str(power['W']) + 'W')
-	#self.powerprogbar.setFormat('23')
+        powerperc = power['W']*100/8.0
+        self.powerprogbar.setValue(powerperc)
+        self.powerprogbar.setFormat(str(power['W']) + 'W')
+        #self.powerprogbar.setFormat('23')
 
 
     def closeEvent(self, x):
