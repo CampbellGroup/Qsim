dc1232c0f1482a1e04db10b5823894770f97386f
aransfor <ransfordanthony@gmail.com>
2016-06-15 10:36:25 -0700

    changed reacto import for new labrad

--- clients/cameraswitch/cameraswitch.py
+++ clients/cameraswitch/cameraswitch.py
@@ -64,7 +64,7 @@ class cameraswitch(QtGui.QWidget):
         
 if __name__=="__main__":
     a = QtGui.QApplication( [] )
-    from common.lib.clients import qt4reactor
+    import qt4reactor
     qt4reactor.install()
     from twisted.internet import reactor
     cameraswitchWidget = cameraswitch(reactor)
