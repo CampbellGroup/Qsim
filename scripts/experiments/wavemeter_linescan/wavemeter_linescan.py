1493f158a814ba130744660044fc2331be12fd27
aransfor <ransfordanthony@gmail.com>
2016-06-15 10:37:14 -0700

    updated wavemeter linescan to work with the new grapher

--- scripts/experiments/wavemeter_linescan/wavemeter_linescan.py
+++ scripts/experiments/wavemeter_linescan/wavemeter_linescan.py
@@ -1,46 +1,70 @@
 import labrad
 from labrad.units import WithUnit
-from common.lib.servers.abstractservers.script_scanner.scan_methods import experiment
+from common.lib.servers.abstractservers.script_scanner.scan_methods \
+    import experiment
+
+
 class wavemeter_linescan(experiment):
-    
+
     name = 'Wavemeter Line Scan'
-    
+
     exp_parameters = []
     exp_parameters.append(('wavemeterscan', 'lasername'))
+
+    exp_parameters.append(('wavemeterscan', 'Port_369'))
+    exp_parameters.append(('wavemeterscan', 'Port_399'))
+    exp_parameters.append(('wavemeterscan', 'Port_935'))
+
+    exp_parameters.append(('wavemeterscan', 'Center_Frequency_369'))
+    exp_parameters.append(('wavemeterscan', 'Center_Frequency_399'))
+    exp_parameters.append(('wavemeterscan', 'Center_Frequency_935'))
 
     @classmethod
     def all_required_parameters(cls):
         return cls.exp_parameters
-    
+
     def initialize(self, cxn, context, ident):
         self.ident = ident
-        self.cxn = labrad.connect(name = 'Wavemeter Line Scan')
-        self.cxnwlm = labrad.connect('10.97.112.2', name = 'Wavemeter Scan')
-        self.wm = self.cxnwlm.multiplexerserver  
-        self.dv = self.cxn.data_vault      
-        self.pmt = self.cxn.normalpmtflow
-        self.laserdict = {'369':5,'935':6,'399':8}
-        self.centerdict = {'369':WithUnit(405.645745,'THz'),'399':WithUnit(751.527140,'THz'),'935':WithUnit(320.571975,'THz')}
-        
-    def run(self, cxn, context):
-        
         self.laser = self.parameters.wavemeterscan.lasername
-        self.laserport = self.laserdict[self.laser]       
-        self.centerfrequency = self.centerdict[self.laser]
-        
-        self.dv.cd('Wavemeter Line Scan', True)
-        self.dv.new('Wavemeter Line Scan',[('freq', 'Hz')], [('', 'Amplitude','kilocounts/sec')])
-        window_name = ['Wavemeter Line Scan']
 
-        self.dv.add_parameter('Window', window_name)
-        self.dv.add_parameter('plotLive', True)
+        if self.laser == '369':
+            self.port = self.parameters.wavemeterscan.Port_369
+            self.centerfrequency = \
+                self.parameters.wavemeterscan.Center_Frequency_369
+
+        elif self.laser == '399':
+            self.port = self.parameters.wavemeterscan.Port_399
+            self.centerfrequency = \
+                self.parameters.wavemeterscan.Center_Frequency_399
+
+        elif self.laser == '935':
+            self.port = self.parameters.wavemeterscan.Port_935
+            self.centerfrequency = \
+                self.parameters.wavemeterscan.Center_Frequency_935
+
+        self.cxn = labrad.connect(name='Wavemeter Line Scan')
+        self.cxnwlm = labrad.connect('10.97.112.2',
+                                     name='Wavemeter Scan', password='lab')
+        self.wm = self.cxnwlm.multiplexerserver
+        self.dv = self.cxn.data_vault
+        self.grapher = self.cxn.grapher
+        self.pmt = self.cxn.normalpmtflow
+
+    def run(self, cxn, context):
+
+        self.dv.cd('Wavemeter Line Scan', True)
+        dataset = self.dv.new('Wavemeter Line Scan', [('freq', 'Hz')],
+                              [('', 'Amplitude', 'kilocounts/sec')])
+        self.grapher.plot(dataset, 'spectrum', False)
+
         self.dv.add_parameter('Frequency', self.centerfrequency)
+        self.dv.add_parameter('Laser', self.laser)
 
         self.currentfreq = self.currentfrequency()
         tempdata = []
         while True:
             should_stop = self.pause_or_stop()
-            if should_stop: 
+            if should_stop:
                 tempdata.sort()
                 self.dv.add(tempdata)
                 break
@@ -49,12 +73,10 @@ class wavemeter_linescan(experiment):
             if self.currentfreq and counts:
                 tempdata.append([self.currentfreq['GHz'], counts])
 
-            
-            
-            
     def currentfrequency(self):
-        self.currentfreq = WithUnit(float(self.wm.get_frequency(self.laserport)), 'THz') - self.centerfrequency
-        
+        absfreq = WithUnit(float(self.wm.get_frequency(self.port)), 'THz')
+        self.currentfreq = absfreq - self.centerfrequency
+
     def finalize(self, cxn, context):
         self.cxn.disconnect()
         self.cxnwlm.disconnect()
@@ -62,7 +84,7 @@ class wavemeter_linescan(experiment):
 if __name__ == '__main__':
     cxn = labrad.connect()
     scanner = cxn.scriptscanner
-    exprt = wavemeter_linescan(cxn = cxn)
+    exprt = wavemeter_linescan(cxn=cxn)
     ident = scanner.register_external_launch(exprt.name)
     exprt.execute(ident)
 
