315a9f5f81db0c23c7d06c7a60be6c7f9da0a112
aransfor <ransfordanthony@gmail.com>
2016-07-07 16:23:15 -0700

    updated comments

--- servers/Arduino_DAC/firmware/arduino_dac/arduino_dac.ino
+++ servers/Arduino_DAC/firmware/arduino_dac/arduino_dac.ino
@@ -40,41 +40,29 @@ void loop()
   chan = Serial.read();
   value = Serial.read();
   /* 
-  insert read function
-  Here we need a serial read to get a input number between 0 and 4096 to be parsed in binary
-  */
-  
-  /*
-  The following function Needs to iterate over the 4 most significant bits of input number in binary
-  and output them to pin SPO. This specifies the channel from 1-8.This is currently hard coded to output 0b0001 (DAC A) with the largest
- allowed values are 0b0001 to (DAC A) to 0b1000 (DAC H) 
+  The following iterates over the 4 most significant bits of input number in binary
+  and output them to pin SPO. This specifies the channel from 1-8. Allowed values are 0b0001 to (DAC A) to 0b1000 (DAC H) 
   */
 
-  for (int j = 0; j<4; j++) // output 000
+  for (int j = 0; j<4; j++) //iterate over nibble that is channel number and write it
    {
-     boolean currentchanbit = chan & 9;
-     currentchanbit  = currentchanbit >> 3;
-//     if (currentbit == 1){
-//         Serial.print(1);
-//     }
-//     else{
-//         Serial.print(0);
-//     }
-     chan = chan << 1;
-     digitalWrite(SPO,currentchanbit);
+     boolean currentchanbit = chan & 9; // This zeros out the byte to the left of bit 4 and leaves bit 4 unchanged
+     currentchanbit  = currentchanbit >> 3;//this chops of the right three bits leaving just one bit (bit 4)
+     chan = chan << 1;// shifts channel to the left one so the next bit in the 4 slot was the old 3 bit
+     digitalWrite(SPO,currentchanbit);//writes the current bit
      digitalWrite(clk, HIGH);// The data from SPO is read when clock is HIGH so set SPO before clock pulse
      digitalWrite(clk, LOW);
      digitalWrite(SPO, LOW);
    }
      /*
-     this next loop writes to the dac channel the output voltage from min (0b00000000) to max (0b11111111)
+     this next loop writes to the dac channel the output voltage allowed values: min (0b00000000) to max (0b11111111)
      */
-  for (int i = 0; i<8; i++) // hard coded output 0b11111111 (max voltage)
+  for (int i = 0; i<8; i++) //iterate over value byte
    {
-    boolean currentvaluebit = value & 255;
-    currentvaluebit = currentvaluebit >> 7;
-    value = value << 1;
-    digitalWrite(SPO,currentvaluebit);
+    boolean currentvaluebit = value & 255;// after the bit shift the byte grows by one bit, this chops it off
+    currentvaluebit = currentvaluebit >> 7;// shifts right seven bits away so only left with the 8 bit
+    value = value << 1;//shifts value to the left so 7 bit is now in 8 bit slot
+    digitalWrite(SPO,currentvaluebit);//writes the old 8 bit
     digitalWrite(clk, HIGH);
     digitalWrite(clk, LOW);
     digitalWrite(SPO, LOW); 
