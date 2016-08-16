642785edc7b60db37e45814994e39ef07bc77e4e
jayich <jayich@gmail.com>
2016-08-16 00:38:31 -0700

    Get and set multipole moments by name.

--- clients/dac8718/multipole_moments.py
+++ clients/dac8718/multipole_moments.py
@@ -21,3 +21,38 @@ class MultipoleMoments(object):
         self.M_6 = None
         self.M_7 = None
         self.M_8 = None
+        self._set_multipole_dict()
+
+    def _set_multipole_dict(self):
+        """
+        Useful for getting and setting values by name.
+        """
+        self._multipole_dict = {}
+        self._multipole_dict['M_0'] = self.M_0
+        self._multipole_dict['M_1'] = self.M_1
+        self._multipole_dict['M_2'] = self.M_2
+        self._multipole_dict['M_3'] = self.M_3
+        self._multipole_dict['M_4'] = self.M_4
+        self._multipole_dict['M_5'] = self.M_5
+        self._multipole_dict['M_6'] = self.M_6
+        self._multipole_dict['M_7'] = self.M_7
+        self._multipole_dict['M_8'] = self.M_8
+
+    def get_value(self, name=None):
+        """
+        Get multipole value by name.
+
+        Parameters
+        ----------
+        name: str
+        """
+        return self._multipole_dict[name]
+
+    def set_value(self, name=None, value):
+        """
+        Parameters
+        ----------
+        name: str, name of the multiple moment, 'M_1', for example.
+        value: float, voltage value to set multiple moment to.
+        """
+        self._multipole_dict[name] = value
