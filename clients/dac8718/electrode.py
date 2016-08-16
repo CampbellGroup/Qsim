cdf962ca276883d4017afb5f0282d7058aef93a9
jayich <jayich@gmail.com>
2016-08-16 11:35:10 -0700

    bit and voltage conversions in electrode class.

--- clients/dac8718/electrode.py
+++ clients/dac8718/electrode.py
@@ -8,7 +8,7 @@ class Electrode(object):
         # Nominally the self.currentvalues value below.
 
         # DAC bit value.
-        self.value = None
+        self._bit_value = None
 
     def _set_number(self):
         """
@@ -17,11 +17,29 @@ class Electrode(object):
         number = int(self.name[-1])
         self.number = number
 
-    def get_voltage(self):
-        bit = self.value
-        voltage = (2.2888e-4*bit - 7.5)
+    def _get_voltage_from_bit_value(self):
+        voltage = 2.2888e-4*self._bit_value - 7.5
         return voltage
+
+    def _get_bit_value_from_voltage(self):
+        bit_value = (self._voltage + 7.4)/2.2888e-4
+        return int(bit_value)
 
     @property
     def voltage(self):
-        return self.get_voltage()
+        return self._voltage
+
+    @voltage.setter
+    def voltage(self, value):
+        self._voltage = value
+        self._bit_value = self._get_bit_value_from_voltage()
+
+    @property
+    def bit_value(self):
+        return self._bit_value
+
+    @bit_value.setter
+    def bit_value(self, value):
+        self._bit_value = value
+        new_voltage = self._get_voltage_from_bit_value()
+        self._voltage = new_voltage
