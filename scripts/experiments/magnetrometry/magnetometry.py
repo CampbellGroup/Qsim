92784624aeefdced39431450e07e704335cb14e0
Thomas Dellaert <thomasdellaert@physics.ucla.edu>
2019-11-20 13:14:55 -0800

    minor tweaks before debugging

--- scripts/experiments/magnetrometry/magnetometry.py
+++ scripts/experiments/magnetrometry/magnetometry.py
@@ -30,8 +30,8 @@ class magnetometry(QsimExperiment):
         self.init_currents = np.array(cxn.Keithley_Server.get_applied_voltage_current(2))
 
     def run(self, cxn, context):
-        self.setup_datavault('current (A)', 'center frequency (MHz)') # gives the x and y names to datavault
-        self.setup_grapher('magnetometry') # sets up the grapher tab
+        self.setup_datavault('current (A)', 'center frequency (MHz)')
+        self.setup_grapher('magnetometry')  # sets up the grapher tab
         self.setup_parameters()
         if   self.coil_direction == 'Bx':
             x_values = self.get_scan_list(self.p.magnetometry.current_scan_x, units=A)
@@ -43,15 +43,15 @@ class magnetometry(QsimExperiment):
         for i, current_step in enumerate(x_values):
             self.currents[self.coil_index] = current_step
             self.ks.all_current(self.currents)
-            dataset, should_break = self.linescan.run(cxn, context) # run the line scan
-            self.linescan.dv.add_parameter(self.coil_direction, current_step)         
-            fitdata = dataset.getData(limit=None, start=0) # get the data from the linescan   
-            #fitdata = dv.get(limit=None, startOver=True)    #may be that we need to navigate to the saved file in the datavault.
+            dataset, should_break = self.linescan.run(cxn, context)  # run the line scan
+            self.linescan.dv.add_parameter(self.coil_direction, current_step)
+            fitdata = dataset.getData(limit=None, start=0)  # get the data from the linescan
+            # fitdata = dv.get(limit=None, startOver=True)
+            # may be that we need to navigate to the saved file in the datavault.
             xdata = [i[0] for i in fitdata]
             ydata = [i[1] for i in fitdata]
-                # fit the data to a sinc^2 function
-            popt, pcov = curve_fit(sincsq, xdata, ydata) #p0=[max(ydata), 20, None]   # initial guess
-            self.dv.add(current_step, popt[2]) # store the center position of the line
+            popt, pcov = curve_fit(sincsq, xdata, ydata)  # p0=[max(ydata), 20, None]
+            self.dv.add(current_step, popt[2])  # store the center position of the line
             if should_break:
                 return should_break
 
@@ -62,7 +62,7 @@ class magnetometry(QsimExperiment):
 
     def finalize(self, cxn, context):
         self.ks.all_current(self.init_currents)
-        
+
 
 if __name__ == '__main__':
     cxn = labrad.connect()
