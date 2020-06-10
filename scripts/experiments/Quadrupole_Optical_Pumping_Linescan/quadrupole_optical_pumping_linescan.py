b89bb6f59eb58ff1737b879230274ab96f557de4
cromes <conradhr@physics.ucla.edu>
2020-06-10 12:49:00 -0700

    created a frequency scan of the quadrupole optical pumping beam to find the correct DDS frequency to set for quadrupole OP

---
+++ scripts/experiments/Quadrupole_Optical_Pumping_Linescan/quadrupole_optical_pumping_linescan.py
@@ -0,0 +1,71 @@
+import labrad
+import numpy as np
+from Qsim.scripts.pulse_sequences.optical_pumping_point import optical_pumping_point as sequence
+from Qsim.scripts.experiments.qsimexperiment import QsimExperiment
+from labrad.units import WithUnit as U
+
+
+class quadrupole_optical_pumping_linescan(QsimExperiment):
+
+
+    name = 'QuadrupoleOpticalPumpingLinescan'
+
+    exp_parameters = []
+
+    exp_parameters.extend(sequence.all_required_parameters())
+    exp_parameters.remove(('OpticalPumping', 'duration'))
+    exp_parameters.remove(('OpticalPumping', 'quadrupole_op_detuning'))
+    exp_parameters.append(('OpticalPumping', 'quadrupole_op_duration'))
+    exp_parameters.append(('QuadrupoleOpticalPumpingLinescan', 'scan'))
+    exp_parameters.append(('OpticalPumping', 'method'))
+    exp_parameters.append(('ShelvingStateDetection', 'repititions'))
+    exp_parameters.append(('ShelvingStateDetection', 'state_readout_threshold'))
+    exp_parameters.append(('StandardStateDetection', 'repititions'))
+    exp_parameters.append(('StandardStateDetection', 'state_readout_threshold'))
+    exp_parameters.append(('Shelving_Doppler_Cooling', 'doppler_counts_threshold'))
+    exp_parameters.append(('Modes', 'state_detection_mode'))
+
+    def initialize(self, cxn, context, ident):
+        self.ident = ident
+
+    def run(self, cxn, context):
+        self.setup_datavault('frequency', 'probability')
+        self.setup_grapher('QuadrupoleOpticalPumpingLinescan')
+        self.frequencies = self.get_scan_list(self.p.QuadrupoleOpticalPumpingLinescan.scan, 'MHz')
+        for i, freq in enumerate(self.frequencies):
+            should_break = self.update_progress(i/float(len(self.frequencies)))
+            if should_break:
+                break
+            self.p['OpticalPumping.method'] = 'QuadrupoleOnly'
+            if self.p.Modes.state_detection_mode == 'Shelving':
+                self.p['OpticalPumping.duration'] = U(0.0, 'us')
+                self.p['OpticalPumping.quadrupole_op_duration'] = self.p.OpticalPumping.quadrupole_op_duration
+                self.p['OpticalPumping.quadrupole_op_detuning'] = U(freq, 'MHz')
+                self.p['Modes.state_detection_mode'] = 'Shelving'
+                self.program_pulser(sequence)
+                [doppler_counts, detection_counts] = self.run_sequence(num=2, max_runs=500)
+                deshelving_errors = np.where(doppler_counts <= self.p.Shelving_Doppler_Cooling.doppler_counts_threshold)
+                detection_counts = np.delete(detection_counts, deshelving_errors)
+            elif self.p.Modes.state_detection_mode == 'Standard':
+                self.p['OpticalPumping.duration'] = U(0.0, 'us')
+                self.p['OpticalPumping.quadrupole_op_duration'] = self.p.OpticalPumping.quadrupole_op_duration
+                self.p['OpticalPumping.quadrupole_op_detuning'] = U(freq, 'MHz')
+                self.p['Modes.state_detection_mode'] = 'Standard'
+                self.program_pulser(sequence)
+                [detection_counts] = self.run_sequence(num=1, max_runs=1000)
+
+            hist = self.process_data(detection_counts)
+            self.plot_hist(hist)
+            pop = self.get_pop(detection_counts)
+            self.dv.add(freq, pop)
+
+    def finalize(self, cxn, context):
+        pass
+
+
+if __name__ == '__main__':
+    cxn = labrad.connect()
+    scanner = cxn.scriptscanner
+    exprt = quadrupole_optical_pumping_linescan(cxn=cxn)
+    ident = scanner.register_external_launch(exprt.name)
+    exprt.execute(ident)
--- servers/cavity_piezo_lock/cavity_piezo_lock.py
+++ servers/cavity_piezo_lock/cavity_piezo_lock.py
@@ -11,7 +11,7 @@ cmdline = %PYTHON% %FILE%
 timeout = 100
 
 [shutdown]
-message = 987654321
+message = 98765432
 timeout = 100
 ### END
 
@@ -25,6 +25,8 @@ from twisted.internet.task import LoopingCall
 
 import socket
 import numpy as np
+import time
+
 
 class cavity_piezo_lock(LabradServer):
 
@@ -35,6 +37,8 @@ class cavity_piezo_lock(LabradServer):
         self.name = socket.gethostname() + ' Cavity Piezo Lock'
         self.chan = 1
         self.rate = 2
+        self.start_time = time.time()
+        self.sleep_time = 10
         self.voltage_history = []
         self.connect()
         self.lc = LoopingCall(self.loop)
@@ -68,16 +72,26 @@ class cavity_piezo_lock(LabradServer):
             set_voltage = init_voltage - U(delta_voltage, 'V')
             yield self.piezo.set_voltage(self.chan, set_voltage)
             self.voltage_history.append(set_voltage['V'])
+            time.sleep(self.sleep_time)
         elif (delta > 0.0) and (np.abs(delta) < 15.0):
             delta_voltage = np.abs(delta)/10.0  # the cavity piezo is roughly 10 MHz/Volt
             set_voltage = init_voltage + U(delta_voltage, 'V')
             yield self.piezo.set_voltage(self.chan, set_voltage)
             self.voltage_history.append(set_voltage['V'])
+            time.sleep(self.sleep_time)
 
     @setting(3014, 'get_voltage_history', returns='*v[]')
     def get_voltage_history(self, c):
         yield None
         returnValue(self.voltage_history)
+
+    @setting(3015, 'set_sleep_time', sleep_time='i')
+    def set_sleep_time(self, c, sleep_time):
+        self.sleep_time = sleep_time
+
+    @setting(3016, 'set_lock_frequency', lock_frequency='v')
+    def set_lock_frequency(self, c, lock_frequency):
+        self.set_point = lock_frequency
 
 if __name__ == "__main__":
     from labrad import util
