9d58f319cae49f66837efeda23fb7ade6560eeee
cromes <conradhr@physics.ucla.edu>
2020-06-25 09:26:24 -0700

    moving crystalsim to the analysis repo

--- crystalsim/objects.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-class ion():
-    def __init__(self, x0=0, y0=0, color='b', constant=False):
-        self.x0 = x0
-        self.x = self.x0
-        self.y0 = y0
-        self.y = self.y0
-        self.constant = constant
-        self.color = color
