b4bac1b0640d59420b6e84fc24bf9f19de469839
thomasdellaert <thomasdellaert@physics.ucla.edu>
2021-03-15 16:07:52 -0700

    Manifold detection experiment rough draft

--- scripts/experiments/Manifold_Detection/manifold_detection.py
+++ scripts/experiments/Manifold_Detection/manifold_detection.py
@@ -1,5 +1,6 @@
 import labrad
 from Qsim.scripts.experiments.qsimexperiment import QsimExperiment
+from Qsim.scripts.pulse_sequences.sub_sequences.ManifoldStateDetection import manifold_state_detection as sequence
 from labrad.units import WithUnit as U
 import time
 import numpy as np
@@ -18,51 +19,84 @@ class off_resonant_shelving_measurement(QsimExperiment):
     def initialize(self, cxn, context, ident):
 
         self.ident = ident
-        self.pulser = self.cxn.pulser
-        self.pmt = self.cxn.normalpmtflow
+        self.pulser = cxn.pulser
+        self.pmt = cxn.normalpmtflow
+        self.pzt_server = cxn.piezo_server
 
     def run(self, cxn, context):
 
         self.setup_datavault('event_num', 'wait_time') #TODO: update this
-        # self.setup_grapher('Manifold Detection')
-        threshold = self.set_single_ion_threshold()
-        status = True
-        N = 1  # this tracks events
 
-        self.program_pulser(single_ion_shelving_attempt)
+        self.program_pulser(sequence)
 
+        still_there = True
+        while still_there:
+            init_time = time.time()
 
+            should_break = self.update_progress(np.random.random())
+            if should_break:
+                break
 
-        counts = self.pmt.get_next_counts('ON', True)
-        init_time = time.time()
+            # check that the ion is still there
+            still_there = self.rescue_ion(5.0)
 
-        # try to shelve an ion until it works
-        is_shelved = False
-        while is_shelved is False:
-            is_shelved = self.attempt_shelving
-        # set the brightness threshold
+            # try to shelve an ion until it works
+            is_shelved = False
+            while is_shelved is False:
+                is_shelved = self.attempt_shelving
 
-        ## START CORE LOOP
+            # set the brightness threshold
+            self.fluorescence, counts = self.get_average_counts()
+            print('initial fluorescence is {} counts'.format(self.fluorescence))
+            self.current_fluorescence = self.fluorescence
 
-        # run for 5 minutes or until fluorescence leaves acceptable range, binning the data and outputting it to datavault
+            # run for 5 minutes or until fluorescence leaves acceptable range, binning the data and outputting it to datavault
+            while (time.time() - init_time < 300) or abs(self.current_fluorescence - self.fluorescence) < self.p.manifoldDetection.cavity_threshold
+                should_break = self.update_progress(np.random.random())
+                if should_break:
+                    break
+                self.current_fluorescence, counts = self.get_average_counts()
+                hist = self.process_data(counts)
+                self.plot_hist(hist, folder_name='ManifoldDetection')
 
-        # move fluorescence back to middle of range
+            # move fluorescence back to middle of range
+            self.correct_cavity_drift() #TODO: define this function
 
-        # check that the ion is still there
+    def get_average_counts(self):
+        counts = self.run_sequence(max_runs=1000, num=1)
+        avg = np.mean(counts)
+        return avg, counts
 
-        # reshelve the ion
-
-        # set new threshold (in case the other ion has a different brightness)
-
-        ## END CORE LOOP
+    def correct_cavity_drift(self):
+        delta = self.current_fluorescence - self.fluorescence
+        j = 0
+        while np.abs(delta) > self.p.manifoldDetection.cavity_threshold:
+            if j > 10:
+                return False
+            # take advantage of the fact that +voltage=red, -voltage=blue if we're red of the line
+            new_cavity_voltage = self.cavity_voltage + 0.01 * np.sign(delta)
+            print('Moving cavity from {} V to {} V'.format(self.cavity_voltage, new_cavity_voltage))
+            self.pzt_server.set_voltage(self.cavity_chan, U(new_cavity_voltage, 'V'))
+            self.cavity_voltage = new_cavity_voltage
+            self.current_fluorescence, counts = self.get_average_counts()
+            delta = self.current_fluorescence - self.fluorescence
+        return True
 
     def attempt_shelving(self):
-
+        self.toggle_repump_lasers('Off')
+        before_counts = np.mean(self.pmt.get_next_counts('ON', True))
+        self.toggle_shelving_laser('On')
+        time.sleep(self.p.manifoldDetection.shelving_attempt_time) #TODO: add this to the parameter vault
+        self.toggle_shelving_laser('Off')
+        after_counts = np.mean(self.pmt.get_next_counts('ON', True))
+        if after_counts < before_counts * 0.6:
+            return True
+        return False
 
     def rescue_ion(self, threshold):
         self.toggle_repump_lasers('On')
         self.toggle_shelving_laser('Off')
-        time.sleep(10.0)
+        time.sleep(self.p.manifoldDetection.rescue_time) #should default to 10 seconds
         counts = self.pmt.get_next_counts('ON', True)
         if counts > threshold:
             return True
---
+++ scripts/pulse_sequences/sub_sequences/ManifoldStateDetection.py
@@ -0,0 +1,56 @@
+from common.lib.servers.Pulser2.pulse_sequences.pulse_sequence import pulse_sequence
+from labrad.units import WithUnit as U
+
+
+class manifold_state_detection(pulse_sequence):
+
+    required_parameters = [
+        ('ManifoldDetection', 'duration'),
+        ('ShelvingStateDetection', 'repump_power'),
+        ('ShelvingStateDetection', 'detuning'),
+        ('ShelvingStateDetection', 'CW_power'),
+        ('ShelvingStateDetection', 'repetitions'),
+        ('ShelvingStateDetection', 'state_readout_threshold'),
+        ('MicrowaveInterrogation', 'power'),
+        ('Transitions', 'main_cooling_369'),
+        ('ddsDefaults', 'doppler_cooling_freq'),
+        ('ddsDefaults', 'doppler_cooling_power'),
+        ('ddsDefaults', 'repump_935_freq'),
+        ('ddsDefaults', 'qubit_dds_freq'),
+        ('ddsDefaults', 'DP369_freq'),
+        ('ddsDefaults', 'protection_beam_freq'),
+        ('ddsDefaults', 'protection_beam_power')
+    ]
+
+    def sequence(self):
+        p = self.parameters
+
+        # standard readout count TTL, provides number of detected photons
+        self.addTTL('ReadoutCount',
+                    self.start,
+                    p.ManifoldDetection.duration)
+
+        # # provides timetags
+        # self.addTTL('TimeResolvedCount',
+        #             self.start,
+        #             p.ShelvingStateDetection.duration)
+
+        self.addDDS('935SP',
+                    self.start,
+                    p.ManifoldDetection.duration,
+                    p.ddsDefaults.repump_935_freq,
+                    p.ShelvingStateDetection.repump_power)
+
+        self.addDDS('369DP',
+                    self.start,
+                    p.ManifoldDetection.duration,
+                    p.Transitions.main_cooling_369/2.0 + p.ddsDefaults.DP369_freq + p.ShelvingStateDetection.detuning/2.0,
+                    p.ShelvingStateDetection.CW_power)
+
+        self.addDDS('DopplerCoolingSP',
+                    self.start,
+                    p.ManifoldDetection.duration,
+                    p.ddsDefaults.doppler_cooling_freq,
+                    p.ddsDefaults.doppler_cooling_power)
+
+        self.end = self.start + p.ShelvingStateDetection.duration
