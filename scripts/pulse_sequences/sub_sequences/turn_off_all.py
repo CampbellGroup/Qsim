2526c205182c30eac7f3cdfa802e74ac47e80123
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-08 14:45:55 -0700

    major refactor of the pulser code. Changes ttls and ddses are now on equal footing with regard to function names error messages are more helpful configuration is consolidated into one file, and names have been changed to better reflect their purpose

--- scripts/pulse_sequences/sub_sequences/turn_off_all.py
+++ scripts/pulse_sequences/sub_sequences/turn_off_all.py
@@ -1,12 +1,12 @@
 from common.lib.servers.Pulser2.pulse_sequences.pulse_sequence import PulseSequence
-from config.pulser.hardwareConfiguration import hardwareConfiguration as hc
+from config.pulser_config import PulserConfiguration as hc
 from labrad.units import WithUnit
 
 
 class TurnOffAll(PulseSequence):
     def sequence(self):
         dur = WithUnit(1., 'us')
-        for channel in hc.ddsDict.keys():
+        for channel in hc.dds_channel_dict.keys():
             if channel not in ['RF_Drive', 'Microwave_qubit']:
                 self.add_dds(channel, self.start, dur,
                              WithUnit(0, 'MHz'), WithUnit(-46.0, 'dBm'))
