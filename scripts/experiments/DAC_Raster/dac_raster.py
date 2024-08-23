c2961d034c4febc98580d7d7fcfec982b075d9c7
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-22 17:15:07 -0700

    made dac raster work much better

--- scripts/experiments/DAC_Raster/dac_raster.py
+++ scripts/experiments/DAC_Raster/dac_raster.py
@@ -8,6 +8,7 @@ allow_concurrent = []
 """
 import labrad
 from Qsim.scripts.experiments.qsimexperiment import QsimExperiment
+from Qsim.scripts.experiments.Interleaved_Linescan.interleaved_linescan import InterleavedLinescan
 from config.dac_ad660_config import HardwareConfiguration as HC
 from twisted.internet.defer import inlineCallbacks
 import time
@@ -29,23 +30,25 @@ class DACRaster(QsimExperiment):
     """
     This experiment will scan through a user specified list of DAC voltages and can be run when
     first trying to load the trap, or if the loadable position in the trap has been lost.
-"""
+    """
     name = 'DAC Raster'
 
     exp_parameters = []
     exp_parameters.append(('dacraster', 'RF1_scan'))
-    exp_parameters.append(('dacraster', 'RF2_scan'))
     exp_parameters.append(('dacraster', 'DC1_scan'))
-    exp_parameters.append(('dacraster', 'DC2_scan'))
     exp_parameters.append(('dacraster', 'Delay_After_DAC_Change'))
     exp_parameters.append(('dacraster', 'PMT_counts_to_average'))
+    exp_parameters.extend(InterleavedLinescan.all_required_parameters())
 
     def initialize(self, cxn, context, ident):
         self.ident = ident
-        self.DACs = self.cxn.dac_ad660_server #connect to DAC server
+        self.linescan = self.make_experiment(InterleavedLinescan)
+        self.linescan_context = self.dv.context()
+        self.linescan.initialize(cxn, self.linescan_context, ident)
+        self.DACs = self.cxn.dac_ad660_server  # connect to DAC server
         self.elec_dict = HC.elec_dict
-        self.data = []
         self.electrodes = {}
+        self.setup_datavault()
 
         for i, (key, channel) in enumerate(self.elec_dict.items()):
             electrode = Electrode(channel.dac_channel_number,
@@ -54,53 +57,62 @@ class DACRaster(QsimExperiment):
                                   name=channel.name)
             self.electrodes[electrode.name] = electrode
 
-        self.RF1_values = self.get_scan_list(self.p['dacraster.RF1_scan'], 'V')
-        self.RF2_values = self.get_scan_list(self.p['dacraster.RF2_scan'], 'V')
-        self.DC1_values = self.get_scan_list(self.p['dacraster.DC1_scan'], 'V')
-        self.DC2_values = self.get_scan_list(self.p['dacraster.DC2_scan'], 'V')
+        self.RF_values = self.get_scan_list(self.p['dacraster.RF1_scan'], 'V')
+        self.DC_values = self.get_scan_list(self.p['dacraster.DC1_scan'], 'V')
 
-        self.total_step = (len(self.RF1_values)) * (len(self.RF2_values)) * (len(self.DC1_values)) * (len(self.DC2_values))
+        self.total_step = len(self.RF_values) * len(self.DC_values)
 
     def run(self, cxn, context):
 
         step_count = 0
 
-        for RF1_voltage in self.RF1_values:
-            self.update_dac(RF1_voltage, self.electrodes['RF Rod 1'])
+        for RF_voltage in self.RF_values:
+            self.update_dac(RF_voltage/2, self.electrodes['RF Rod 1'])
+            self.update_dac(-RF_voltage/2, self.electrodes['RF Rod 2'])
 
-            for RF2_voltage in self.RF2_values:
-                self.update_dac(RF2_voltage, self.electrodes['RF Rod 2'])
+            for DC_voltage in self.DC_values:
+                step_count += 1
+                progress = step_count / self.total_step
+                self.update_dac(DC_voltage/2, self.electrodes['DC Rod 1'])
+                self.update_dac(-DC_voltage/2, self.electrodes['DC Rod 2'])
 
-                for DC1_voltage in self.DC1_values:
-                    self.update_dac(DC1_voltage, self.electrodes['DC Rod 1'])
+                time.sleep(self.p['dacraster.Delay_After_DAC_Change']['s'])
+                should_break = self.take_data(progress, cxn,
+                                              (RF_voltage/2, DC_voltage/2, -DC_voltage/2, -RF_voltage/2))
+                if should_break:
+                    return True
 
-                    for i, DC2_voltage in enumerate(self.DC2_values):
-                        step_count += 1
-                        progress = step_count / self.total_step
-#                        should_break = self.update_progress(progress)
-#                        if should_break:
-#                            return should_break
-
-                        self.update_dac(DC2_voltage, self.electrodes['DC Rod 2'])
-
-                        time.sleep(self.p['dacraster.Delay_After_DAC_Change']['s'])
-                        self.take_data(progress)
-
-
-    def take_data(self, progress):
-
+    def take_data(self, progress, cxn, voltages):
         should_break = self.update_progress(progress)
         if should_break:
-            self.dv.add(self.data)
             return True
-        counts = self.pmt.get_next_counts('ON', int(self.p['dacraster.PMT_counts_to_average']), True)
-        current_DAC_voltages = self.DACs.get_analog_voltages()
-        self.data.append([current_DAC_voltages, counts])
+        (center, fwhm, scale, offset), _ = self.linescan.run(cxn, self.linescan_context)
+        current_dac_voltages = [v[1] for v in self.DACs.get_current_voltages()][4:]
+
+        self.dv.add(*current_dac_voltages, fwhm, context=self.context)
+
+        # counts = self.pmt.get_next_counts('ON', int(self.p['dacraster.PMT_counts_to_average']), True)
+        # self.data.append([current_dac_voltages, counts])
+
+    def setup_datavault(self):
+        """
+        Adds parameters to datavault and parameter vault
+        """
+        self.dv.cd(['', self.name], True)
+        self.dataset = self.dv.new(self.name, [("RF1", 'num'),
+                                   ("DC1", 'num'),
+                                   ("DC2", 'num'),
+                                   ("RF2", 'num')],
+                                   [("fwhm", '', 'num')], context=self.context)
+        for parameter in self.p:
+            self.dv.add_parameter(parameter, self.p[parameter], context=self.context)
+        return self.dataset
 
 
     @inlineCallbacks
     def update_dac(self, voltage, electrode):
         yield self.DACs.set_individual_analog_voltages([(electrode.dac, voltage)])
+
 
 if __name__ == '__main__':
     cxn = labrad.connect()
