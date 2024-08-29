67b845a89d072cd69a15acbfad1f0998db41fe5d
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-29 14:13:42 -0700

    pv api change

--- scripts/pulse_sequences/microwave_point/microwave_point.py
+++ scripts/pulse_sequences/microwave_point/microwave_point.py
@@ -1,21 +1,21 @@
-from common.lib.servers.Pulser2.pulse_sequences.pulse_sequence import PulseSequence
+from Qsim.scripts.pulse_sequences.sub_sequences.deshelving import Deshelving
 from Qsim.scripts.pulse_sequences.sub_sequences.doppler_cooling import DopplerCooling
 from Qsim.scripts.pulse_sequences.sub_sequences.microwave_interrogation.microwave_interrogation import (
     MicrowaveInterrogation,
 )
-from Qsim.scripts.pulse_sequences.sub_sequences.turn_off_all import TurnOffAll
-from Qsim.scripts.pulse_sequences.sub_sequences.state_detection.standard_state_detection import (
-    StandardStateDetection,
+from Qsim.scripts.pulse_sequences.sub_sequences.optical_pumping import OpticalPumping
+from Qsim.scripts.pulse_sequences.sub_sequences.shelving import Shelving
+from Qsim.scripts.pulse_sequences.sub_sequences.shelving_doppler_cooling import (
+    ShelvingDopplerCooling,
 )
 from Qsim.scripts.pulse_sequences.sub_sequences.state_detection.shelving_state_detection import (
     ShelvingStateDetection,
 )
-from Qsim.scripts.pulse_sequences.sub_sequences.shelving_doppler_cooling import (
-    ShelvingDopplerCooling,
+from Qsim.scripts.pulse_sequences.sub_sequences.state_detection.standard_state_detection import (
+    StandardStateDetection,
 )
-from Qsim.scripts.pulse_sequences.sub_sequences.optical_pumping import OpticalPumping
-from Qsim.scripts.pulse_sequences.sub_sequences.shelving import Shelving
-from Qsim.scripts.pulse_sequences.sub_sequences.deshelving import Deshelving
+from Qsim.scripts.pulse_sequences.sub_sequences.turn_off_all import TurnOffAll
+from common.lib.servers.Pulser2.pulse_sequences.pulse_sequence import PulseSequence
 
 
 class MicrowavePoint(PulseSequence):
@@ -38,7 +38,7 @@ class MicrowavePoint(PulseSequence):
 
     def sequence(self):
         p = self.parameters
-        mode = p.Modes.state_detection_mode
+        mode = p["Modes.state_detection_mode"]
 
         self.add_sequence(TurnOffAll)
 
